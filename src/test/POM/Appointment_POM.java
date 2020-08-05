753dae499eacb5a2b5af60e0e58c118e5091931b
nelsiki <bryanwrb@gmail.com>
2020-08-05 12:21:36 -0400

    Appointment_POM Create a sales appointment of 1 minute Assign to 193 sc1 Confirmed appointment & virtual consultation checked Save and close
    Test_NewLeadOpp
    Test case added

--- src/test/POM/Appointment_POM.java
+++ src/test/POM/Appointment_POM.java
@@ -1,10 +1,13 @@
 package POM;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Appointment_POM {
 
@@ -14,6 +17,11 @@ public class Appointment_POM {
     By resource = By.id("bfr_ressource");
     By resourceField = By.id("bfr_ressource_ledit");
     By resourceMagnifyingGlass = By.id("bfr_ressource_i");
+    By appointmentConfirmedCheckBox = By.id("bfr_appointmentconfirmed_i");
+    By virtualConsultationCheckBox = By.id("bfr_virtualmeeting_i");
+    By saveAndCloseButton = By.cssSelector(".ms-crm-CommandBarItem.ms-crm-CommandBar-Menu.ms-crm-CommandBar-Button");
+    By duration = By.id("scheduleddurationminutes");
+    By durationField = By.id("scheduleddurationminutes_iSelectInput");
 
     public Appointment_POM(WebDriver driver) {
         this.driver = driver;
@@ -42,5 +50,29 @@ public class Appointment_POM {
         driver.findElement(resourceField).sendKeys("193 sc1");
         driver.findElement(resourceMagnifyingGlass).click();
         driver.findElement(By.xpath("//li[@id='item0']/a[2]/span/nobr/div/div[3]")).click();
+    }
+
+    public void selectAppointmentConfirmed() {
+        driver.findElement(appointmentConfirmedCheckBox).click();
+    }
+
+    public void selectVirtualConsultation() {
+        driver.findElement(virtualConsultationCheckBox).click();
+    }
+
+    public void saveAndClose() {
+        List<WebElement> topMenuBar = driver.findElements((saveAndCloseButton));
+        for (int i = 0; i < topMenuBar.size(); i++) {
+            System.out.println("topMenuBar :" + topMenuBar.get(i));
+        }
+        topMenuBar.get(1).click();
+    }
+
+    public void setAppointmentDuration(String time) {
+        driver.findElement(duration).click();
+        driver.findElement(durationField).sendKeys(Keys.CONTROL, "a");
+        driver.findElement(durationField).sendKeys(Keys.BACK_SPACE);
+        driver.findElement(durationField).sendKeys(time);
+        driver.findElement(durationField).sendKeys(Keys.ENTER);
     }
 }
--- src/test/TestScenarios/Test_NewLeadOpp.java
+++ src/test/TestScenarios/Test_NewLeadOpp.java
@@ -1,25 +1,37 @@
 package TestScenarios;
 
 import Config.Config;
-import Config.DashBoard_POM;
+import Config.Generators;
+import POM.Appointment_POM;
+import POM.DashBoard_POM;
 import POM.LeadOpportunity_POM;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.awt.*;
 
 public class Test_NewLeadOpp extends Config {
 
+    public Generators generators;
+
+    @BeforeTest
+    public void initiate() {
+        objDashBoard = new DashBoard_POM(driver);
+        objLeadOpportunity = new LeadOpportunity_POM(driver);
+        objAppointment = new Appointment_POM(driver);
+
+    }
     //go to Next page
     @Test(priority = 1)
     public void goToLeadOpp() throws InterruptedException {
-        objDashBoard = new DashBoard_POM(driver);
+
         objDashBoard.newLeadOpp();
     }
 
     //Create new LeadOpp
     @Test(priority = 2)
     public void createNewLeadOpp() throws InterruptedException, AWTException {
-        objLeadOpportunity = new LeadOpportunity_POM(driver);
+
 
         objLeadOpportunity.switchContentFrame1();
 
@@ -27,7 +39,7 @@ public class Test_NewLeadOpp extends Config {
         objLeadOpportunity.enterBathroomInformation("2000", "1");
 
         // ADDRESS INFORMATION
-        objLeadOpportunity.setServiceAddress("Automation Address ", "Automation Unit ", "Automation City ", "NH", "60148"); //Branch 193
+        objLeadOpportunity.setServiceAddress("Automation Address ", "Automation Unit ", "Automation City ", "NH", "03032"); //Branch 193
 
         // INTEREST IN BATH FITTER  NOT WORKING CURRENTLY
         // objLeadOpportunity.randomInterestInBathFitter();
@@ -43,5 +55,17 @@ public class Test_NewLeadOpp extends Config {
         objLeadOpportunity.enterCustomer(); // All data is automatically generated
         objLeadOpportunity.saveContact();
         objLeadOpportunity.saveLead();
+    }
+
+    @Test(priority = 3)
+    public void createNewSalesAppointment() throws InterruptedException {
+        objLeadOpportunity.createSalesActivity();
+        objAppointment.selectwindow();  // Select sales appointment window
+        objAppointment.selectResource();
+        objAppointment.selectAppointmentConfirmed();
+        objAppointment.selectVirtualConsultation();
+        objAppointment.setAppointmentDuration("1 minute");
+        driver.switchTo().defaultContent();
+        objAppointment.saveAndClose();
     }
 }
